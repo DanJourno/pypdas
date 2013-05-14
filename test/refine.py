202c38e66a9d3fb45a2b77d144f737fe7af4bfee
zhh210 <zhh210@lehigh.edu>
2013-05-14 00:17:21 -0400

    New results.

--- test/refine.py
+++ test/refine.py
@@ -13,23 +13,39 @@ for size in ['b','q','w']:
         exup.append(size+'-exup-'+cond)
         ineup.append(size+'-ineup-'+cond)
 
-print '---cgop----\n'
+print '|---cgop----'
+print '| Call CG to solve subproblems exactly\n'
+print '------------------------------------------------'
+print ' size     cond      iter     cg-iter     time'
+print '------------------------------------------------'
+
 for i in cgop:
     tmp = np.sum(np.genfromtxt(i)[:,1:-1],axis=0)/50
     print '{0:.1e}  {1:.1e}  {2:.2e}  {3:.2e}  {4:.4e}'.format(tmp[0],tmp[1],tmp[2],tmp[3],tmp[4])
 
-print '---exop----\n'
+print '|---exop----'
+print '| Call default to solve subproblems exactly\n'
 for i in exop:
     tmp = np.sum(np.genfromtxt(i)[:,1:-1],axis=0)/50
     print '{0:.1e}  {1:.1e}  {2:.2e}  {3:.4e}'.format(tmp[0],tmp[1],tmp[2],tmp[3])
 
     
-print '---exup----\n'
+
+print '|---exup----'
+print '| Call CG to solve subproblems inexactly but obtain exact update\n'
+print '------------------------------------------------'
+print ' size     cond      iter     cg-iter     time'
+print '------------------------------------------------'
 for i in exup:
     tmp = np.sum(np.genfromtxt(i)[:,1:-1],axis=0)/50
     print '{0:.1e}  {1:.1e}  {2:.2e}  {3:.2e}  {4:.4e}'.format(tmp[0],tmp[1],tmp[2],tmp[3],tmp[4])
 
-print '---ineup----\n'
+
+print '|---ineup----'
+print '| Call CG to solve subproblems inexactly and obtain inexact update\n'
+print '------------------------------------------------'
+print ' size     cond      iter     cg-iter     time'
+print '------------------------------------------------'
 for i in ineup:
     tmp = np.sum(np.genfromtxt(i)[:,1:-1],axis=0)/50
     print '{0:.1e}  {1:.1e}  {2:.2e}  {3:.2e}  {4:.4e}'.format(tmp[0],tmp[1],tmp[2],tmp[3],tmp[4])
