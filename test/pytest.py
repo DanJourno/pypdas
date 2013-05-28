08217490a51432a6ec57a23486629c8308ff8e4d
zhh210 <zhh210@lehigh.edu>
2013-05-28 16:17:56 -0400

    added test file pytest.py

---
+++ test/pytest.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+# Load necessary modules
+from pypdas.utility.random import sprandsym
+from pypdas.bqp.clsbqp import BQP
+from pypdas.algorithms import pdas
+from copy import copy
+
+# Generate a matlab data file containing a BQP with size=10, sparsity=0.5, cond=100 and with name 'test.mat'
+sprandsym(10,0.5,0.01,1,'test')
+a = BQP('test')
+b = copy(a); c = copy(a)
+
+# Run different algorithms to solve the BQP
+pdas.cgopt(a)
+pdas.exupdate(b)
+pdas.inexupdate(c)
